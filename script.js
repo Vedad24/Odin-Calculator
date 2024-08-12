9c3d59dfde0ac628ed3ca94d65ac22a699b3ee87
vedad <vedolino2004@gmail.com>
2024-08-13 00:53:25 +0200

    Added caluclator functionality

---
+++ script.js
@@ -0,0 +1,80 @@
+let operate = (operator, num1, num2) => {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return "Input not ok!";
+    }
+}
+let add = (num1, num2) => {
+    return num1 + num2;
+}
+let subtract = (num1, num2) => {
+    return num1 - num2;
+}
+let multiply = (num1, num2) => {
+    return num1 * num2;
+}
+let divide = (num1, num2) => {
+    return num1 / num2;
+}
+
+let screen = document.querySelector("#screenInput");
+let x = "";
+let op = "";
+let int1;
+let final;
+
+//Functionality of keys with numbers
+document.querySelectorAll(".number").forEach(key => {
+    key.addEventListener("click", keyPress = () => {
+        if (x.length < 10) {
+            x += parseInt(key.innerHTML);
+            screen.innerHTML += key.innerHTML;
+        }    
+    } )
+})
+
+let clearVarScreen = () => {
+    screen.innerHTML = ""; x = "";
+}
+
+//Operators
+document.querySelectorAll(".oper").forEach(oper => {
+    oper.addEventListener("click", operFunc = () => {
+        if (!final) {
+            int1 = parseInt(x);
+        }
+        else {
+            int1 = final;
+        }
+        clearVarScreen();
+        op = oper.innerHTML;
+    })
+})
+
+// Clearing the screen
+document.querySelector(".clear").addEventListener("click", clearFunc = () => {
+    clearVarScreen();
+    final = 0;
+});
+document.querySelector(".del").addEventListener("click", delFun = () => {
+    let s = screen.innerHTML;
+    s = s.slice(0, -1);
+    screen.innerHTML = s;
+    x = x.slice(0, - 1);
+});
+
+
+// Equal
+let equal = () => {
+    final = operate(op, int1, parseInt(x));
+    screen.innerHTML = final;
+}
+document.querySelector(".equal").addEventListener("click", equal)
